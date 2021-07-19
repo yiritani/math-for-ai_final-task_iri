4fbbe5bd9791733070d9658db7d78094b15365dd
yiritani <74131902+yiritani@users.noreply.github.com>
2021-07-19 11:32:43 +0900

    fix true_data shape

--- applications/ML_learning/apps/learning.py
+++ applications/ML_learning/apps/learning.py
@@ -11,7 +11,6 @@ import pathlib
 
 def getCsvData():
     df = pd.read_csv('../templates/house_price.csv')
-    # print(df.head())
 
     return df
 
@@ -20,11 +19,10 @@ def generatePredictModel():
     with open('../templates/house_price.csv', 'r') as fd:
         feature_names = np.array(fd.read().split('\n')[0].split(','))
 
-    learning_data = np.loadtxt('../templates/house_price.csv', delimiter=',', skiprows=1)
-    # print(learning_data[:, np.logical_or(feature_names == 'FarFromStation', feature_names == 'Age')])
+    csv_data = np.loadtxt('../templates/house_price.csv', delimiter=',', skiprows=1)
+    learning_data = csv_data[:, np.logical_or(feature_names == 'FarFromStation', feature_names == 'Age')]
 
-    true_data = np.loadtxt('../templates/house_price.csv', delimiter=',', skiprows=1, usecols=4)
-    # print(true_data)
+    true_data = csv_data[:, feature_names == 'TotalPrice']
 
     return learning_data, true_data
 
@@ -35,8 +33,11 @@ if __name__ == 'learning':
 
     x1, yt = generatePredictModel()
     print(x1)
-    print(yt)
-    # plt.scatter(x_axis, y_axis, s=10, c='b')
-    # plt.xlabel('Far from station', fontsize=14)
-    # plt.ylabel('Price(included management price)', fontsize=14)
-    # plt.show()
+    # print(yt)
+    y = np.insert(yt, 0, 1.0, axis=1)
+    print(y)
+
+    plt.scatter(x1, y, s=10, c='b')
+    plt.xlabel('terms', fontsize=14)
+    plt.ylabel('Price', fontsize=14)
+    plt.show()
