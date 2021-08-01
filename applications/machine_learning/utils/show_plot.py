43c2c406927d570c7436b141f49884b7645eb032
yiritani <74131902+yiritani@users.noreply.github.com>
2021-08-01 23:45:50 +0900

    fix plot label

--- applications/machine_learning/utils/show_plot.py
+++ applications/machine_learning/utils/show_plot.py
@@ -10,12 +10,13 @@ config = config_getter.config_initialize()
 def show_scatter_regression(x, yt, xl, yl, my_home_x=None, my_home_y=None):
     # 散布図と回帰直線の描画
     plt.figure(figsize=(6, 6))
-    plt.scatter(x, yt, s=10, c='b')
+    plt.scatter(x, yt, s=10, c='b', label="group1")
     plt.scatter(my_home_x, my_home_y, s=100, c='r', marker="*")
     plt.xlabel('(FarFromStation / 2) + Age', fontsize=14)
     plt.ylabel('TotalPrice', fontsize=14)
     plt.plot(xl[:, 1], yl, c='k')
     plt.title('Is the price of your home reasonable?')
+    plt.legend(("Linear", "Market price", "My home"), loc="upper left")
 
     # dockerではshow()を使うのが面倒くさいからpngを保存する
     plt.savefig(config_getter.get_png_file_path() + config['FILE']['PNG_FILE_NAME'])
